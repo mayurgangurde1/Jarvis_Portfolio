e226ade0e8696186bb53fd68d55a9b7a12d021dc
mayurgangurde1 <mayur.gangurde@techstalwarts.com>
2023-09-18 10:03:40 +0530

    update test class

--- JARVIS/src/test/java/com/logintest/TestClass_Signup.java
+++ JARVIS/src/test/java/com/logintest/TestClass_Signup.java
@@ -2,6 +2,7 @@ package com.logintest;
 
 import java.io.IOException;
 
+import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 
 import com.Jarvis.Library.UtilityClass;
@@ -31,5 +32,143 @@ public class TestClass_Signup extends BaseTest1 {
 	public void verifyOtpverification() {
 		signup.enterotp();
 	}
+	@Test(priority = 3)
+	public void ClickOnInvestNowButton()
+	{
+			riskprofile.ClickOnInvestNow();
+	}
+	@Test(priority = 4)
+	public void ClickOnStartNowButton()
+	{
+			riskprofile.ClickOnStartnow();
+	}
+	@Test(priority = 5)
+	public void AttemptQuestionnaries() throws InterruptedException
+	{
+				riskprofile.Atteptquestionnaire();
+	}
+	@Test(priority=6)
+	public void ClickOnSubmitMyResponcesButton()
+	{
+			riskprofile.ClickOnSubmitMyResponces();
+	}
+	@Test(priority = 7)
+	public void ConfirmResponces() throws InterruptedException
+	{
+			riskprofile.SubmitMyResponces();
+			Thread.sleep(2000);
+	}
+	@Test(priority =8)
+	public void SelectCheckbox()
+	{
+			riskprofile.SelectCheckbox();
+	}
+	@Test(priority =9)
+	public void ConfirmRiskProfile() 
+	{	
+			riskprofile.ConfirmRiskProfile();
+	}
+	@Test(priority =10)
+	public void SelectTenure1() throws IOException
+	{
+			investmentpreference.selectYear();
+	}
+	@Test(priority =11)
+	public void SelectStrategy()
+	{
+			investmentpreference.StrategySelection();
+	}
+	@Test(priority = 12)
+	public void ClickOnSubmitMyStrategy()
+	{
+			
+		investmentpreference.SubmitStrategy();
+	}
+	@Test(priority =13)
+	public void SelectAmount()
+	{
+			investmentpreference.SelectAmount();
+	}
+	@Test(priority = 14)
+	public void SelectPlan()
+	{
+			investmentpreference.SelectPlan();
+	}
+	@Test(priority = 15)
+	public void CLickOnContinueButton()
+	{
+			investmentpreference.ClickOnContinueButton();
+	}
+	@Test(priority = 16)
+	public void ConfirmInvestmentPreference() throws InterruptedException
+	{
+
+			investmentpreference.ClickConfirmPreference();
+	}
+	@Test(priority =17)
+	public void Clickonboarding() {
+		investmentpreference.clickoncomplteonboarding();
+	}
+	@Test(priority =18)
+	public void UnlockPortfolioButton()
+	{
+			investmentpreference.ClickonunlockNowButtton();
+	}
+	@Test(priority = 19)
+	public void ClickOnPayNowButton()
+	{
+			payment.ClickoPayButton();
+	}
+	@Test(priority = 20)
+	public void ClickOnIAgreeButton()
+	{
+			payment.ClickOnIAgree();
+	}
+	@Test(priority = 21)
+	public void selectpaymentmethod()
+	{
+			payment.Selectpaymentmethod();
+	}
+	@Test(priority = 22)
+	public void EnterUpiID()
+	{
+			payment.enterupi();
+			}
+	@Test(priority = 23)
+	public void CLickonVerifyButton() throws InterruptedException
+	{
+			payment.ClickonVerify();
+			}
+	@Test(priority = 24)
+	public void ClickonPaynowButton()
+	{
+			payment.clickonpaynow();
+			}
+	@Test(priority = 25)
+	public void ClickonPayUsingupi() throws InterruptedException {
+		driver.switchTo().frame(driver.findElement(By.xpath("//iframe[@class='razorpay-checkout-frame']")));
+		//driver.switchTo().frame(driver.findElement(By.xpath("//iframe[@allow='otp-credentials']")));
+		Thread.sleep(5000);
+		payment.ClickOnPayusingUpibutton();
+	}
+	@Test(priority = 26)
+	public void EnterUpiId() throws IOException {
+		payment.EnterUpiId();
+	}
+	@Test(priority = 27)
+	public void ClickOnPayNow() throws InterruptedException {
+		payment.ClickOnPayNow();
+		
+		Thread.sleep(10000);
+	}
+	@Test(priority = 28)
+	public void ClickonContinueButton() throws InterruptedException
+	{
+		Thread.sleep(3000);
+
+		payment.ClickonContinueButton();	
+	}
+}
+
 	
 }
